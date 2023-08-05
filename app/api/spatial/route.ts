bf8af1a2d43d17e935a0cbdc7e08befbac0fc3e4
domsteil <domsteil14@gmail.com>
2023-08-05 16:36:05 -0700

    updating api

--- app/api/spatial/route.ts
+++ app/api/spatial/route.ts
@@ -1,30 +1,18 @@
 import { client } from "@gradio/client";
 
-import Replicate from "replicate";
-import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { data } from "autoprefixer";
 
 export async function POST(
   req: Request
 ) {
   try {
     
-    const { userId } = auth();
     const body = await req.json();
     const { prompt  } = body;
 
     const app = await client("https://85f00cee8ca5c8de88.gradio.live/");
-    const response = await app.predict(2, [	
-                    "facebook/musicgen-large",	
-                    "MultiBand_Decoder",
-                    prompt,
-                    22,
-                    250,
-                    0,
-                    1,
-                    3,
-        ]);
+    
+    const response = await app.predict(2, ["facebook/musicgen-large", "MultiBand_Decoder", prompt, "", 22, 250, 0, 1, 3]);
 
     return NextResponse.json(response);
     
