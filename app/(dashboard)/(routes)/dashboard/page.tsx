c868319a2d380dbfc7525784f768969a40e8faeb
domsteil <domsteil14@gmail.com>
2023-08-07 14:17:33 -0700

    adding music

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -6,36 +6,185 @@ import { useRouter } from "next/navigation";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
-import { tools } from "@/constants";
+import { PlusCircledIcon } from "@radix-ui/react-icons"
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/registry/new-york/ui/tabs"
+
+import { ScrollArea, ScrollBar } from "@/registry/new-york/ui/scroll-area"
+import { Separator } from "@/registry/new-york/ui/separator"
+
+import { AlbumArtwork } from "@/components/album-artwork"
+
+export interface Album {
+  name: string
+  artist: string
+  cover: string
+}
+
+export const listenNowAlbums: Album[] = [
+  {
+    name: "React Rendezvous",
+    artist: "Ethan Byte",
+    cover:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Async Awakenings",
+    artist: "Nina Netcode",
+    cover:
+      "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+  },
+  {
+    name: "The Art of Reusability",
+    artist: "Lena Logic",
+    cover:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Stateful Symphony",
+    artist: "Beth Binary",
+    cover:
+      "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+  },
+]
+
+export const madeForYouAlbums: Album[] = [
+  {
+    name: "Thinking Components",
+    artist: "Lena Logic",
+    cover:
+      "https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Functional Fury",
+    artist: "Beth Binary",
+    cover:
+      "https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80",
+  },
+  {
+    name: "React Rendezvous",
+    artist: "Ethan Byte",
+    cover:
+      "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Stateful Symphony",
+    artist: "Beth Binary",
+    cover:
+      "https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Async Awakenings",
+    artist: "Nina Netcode",
+    cover:
+      "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+  },
+  {
+    name: "The Art of Reusability",
+    artist: "Lena Logic",
+    cover:
+      "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+  },
+]
 
 export default function HomePage() {
+
   const router = useRouter();
 
   return (
     <div>
+      <br/>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Explore the world of generative music
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          Spatial Generation is a platform for generating music based on your landscape and environment using AI.
         </p>
       </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {tools.map((tool) => (
-          <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer">
-            <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-8 h-8", tool.color)} />
-              </div>
-              <div className="font-semibold">
-                {tool.label}
+      <Tabs defaultValue="music" className="h-full space-y-6">
+        <div className="ml-2 space-between flex items-center">
+          <TabsList>
+            <TabsTrigger value="music" className="relative">
+              Music
+            </TabsTrigger>
+          </TabsList>
+          <div className="ml-auto mr-4">
+            <Button>
+              <PlusCircledIcon className="mr-2 h-4 w-4" />
+              <a href="/music">Add music</a>
+            </Button>
+          </div>
+        </div>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          <TabsContent
+            value="music"
+            className="border-none p-0 outline-none"
+          >
+            <div className="flex items-center justify-between">
+              <div className="space-y-1">
+                <h2 className="text-2xl font-semibold tracking-tight">
+                  Listen Now
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                  Top picks for you. Updated daily.
+                </p>
               </div>
             </div>
-            <ArrowRight className="w-5 h-5" />
-          </Card>
-        ))}
-      </div>
+            <Separator className="my-4" />
+            <div className="relative">
+              <ScrollArea>
+                <div className="flex space-x-4 pb-4">
+                  {listenNowAlbums.map((album) => (
+                    <AlbumArtwork
+                      key={album.name}
+                      album={album}
+                      className="w-[250px]"
+                      aspectRatio="portrait"
+                      width={250}
+                      height={330}
+                    />
+                  ))}
+                </div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+            </div>
+            <div className="mt-6 space-y-1">
+              <h2 className="text-2xl font-semibold tracking-tight">
+                Made for You
+              </h2>
+              <p className="text-sm text-muted-foreground">
+                Your personal playlists. Updated daily.
+              </p>
+            </div>
+            <Separator className="my-4" />
+            <div className="relative">
+              <ScrollArea>
+                <div className="flex space-x-4 pb-4">
+                  {madeForYouAlbums.map((album) => (
+                    <AlbumArtwork
+                      key={album.name}
+                      album={album}
+                      className="w-[150px]"
+                      aspectRatio="square"
+                      width={150}
+                      height={150}
+                    />
+                  ))}
+                </div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+            </div>
+          </TabsContent>
+        </div>
+      </Tabs>
     </div>
   );
 }
--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -21,8 +21,9 @@ export async function POST(
       "facebookresearch/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906",
       {
         input: {
-          model_version: "melody",
+          model_version: "large",
           prompt: prompt,
+          duration: 8
         }
       }
     );
